27cd49f05bd6b7db213c854c4f665bf39451f484
cprasatya <caturprstty9991@gmail.com>
2026-01-26 11:04:17 +0700

    feat: add client-side admin verification service

--- src/components/shop/auth/fetchApi.js
+++ src/components/shop/auth/fetchApi.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 const apiURL = process.env.REACT_APP_API_URL;
 
+const BearerToken = () =>
+  localStorage.getItem("jwt")
+    ? JSON.parse(localStorage.getItem("jwt")).token
+    : false;
+const Headers = () => {
+  return {
+    headers: {
+      token: `Bearer ${BearerToken()}`,
+    },
+  };
+};
+
 export const isAuthenticate = () =>
   localStorage.getItem("jwt") ? JSON.parse(localStorage.getItem("jwt")) : false;
 
@@ -8,6 +20,15 @@ export const isAdmin = () =>
   localStorage.getItem("jwt")
     ? JSON.parse(localStorage.getItem("jwt")).user.role === 1
     : false;
+
+export const isAdminCheck = async () => {
+  try {
+    let res = await axios.post(`${apiURL}/api/isadmin`, null, Headers());
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const loginReq = async ({ email, password }) => {
   const data = { email, password };
